c2703d681ada11fd927aaad26c4318fe2f65e739
Nicolas Fuentes <62187757+ArcAegis@users.noreply.github.com>
2023-08-15 13:24:04 -0300

    PreEntrega3

--- index.js
+++ index.js
@@ -1,57 +1,99 @@
-let personas = []
+const modal = document.getElementById('modal');
+const openModalBtn = document.getElementById('openModalBtn');
+const cerrarModalBtn = document.getElementById('cerrarModalBtn');
+const agregarBtn = document.getElementById('agregarBtn');
+const tablaBody = document.getElementById('tabla-body');
+let idCounter = 1;
+let editingRow = null;
 
-const agregarPersonas = ()=>{
-    const nombre = prompt('Ingrese el nombre de la persona');
-    const apellido = prompt('Ingrese el apellido de la persona');
-    const edad = parseInt(prompt('Ingrese la edad de la persona'));
+openModalBtn.addEventListener('click', () => {
+  modal.style.display = 'flex';
+  editingRow = null;
+  clearInputFields();
+});
 
-    const personaExistente = personas.find(p => p.nombre === nombre && p.apellido === apellido && p.edad === edad);
-    if (personaExistente) {
-      alert("Esta persona ya existe en el array, no se agregará de nuevo.");
-    } else {
-      personas.push({
-        nombre: nombre,
-        apellido: apellido,
-        edad: edad
-      });
-      alert(`¡${nombre} ${apellido} ha sido agregado/a al array!`);
+cerrarModalBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+  clearInputFields();
+});
+
+tablaBody.addEventListener('click', event => {
+  if (event.target.classList.contains('editarBtn')) {
+    const id = event.target.getAttribute('data-id');
+    editingRow = tablaBody.querySelector(`tr[data-id="${id}"]`);
+    const nombre = editingRow.querySelector('td:nth-child(2)').textContent;
+    const apellido = editingRow.querySelector('td:nth-child(3)').textContent;
+    const edad = editingRow.querySelector('td:nth-child(4)').textContent;
+
+    document.getElementById('edit-id').value = id;
+    document.getElementById('nombre').value = nombre;
+    document.getElementById('apellido').value = apellido;
+    document.getElementById('edad').value = edad;
+
+    modal.style.display = 'flex';
+  } else if (event.target.classList.contains('borrarBtn')) {
+    const id = event.target.getAttribute('data-id');
+    const rowToDelete = tablaBody.querySelector(`tr[data-id="${id}"]`);
+    tablaBody.removeChild(rowToDelete);
+  }
+});
+
+agregarBtn.addEventListener('click', () => {
+  const nombre = document.getElementById('nombre').value;
+  const apellido = document.getElementById('apellido').value;
+  const edad = document.getElementById('edad').value;
+
+  if (editingRow === null) {
+    // Agregar nueva persona
+    if (nombre && apellido && edad) {
+      const newRow = createPersonRow(idCounter, nombre, apellido, edad);
+      tablaBody.appendChild(newRow);
+      idCounter++;
+      clearInputFields();
+      modal.style.display = 'none';
     }
+  } else {
+    // Editar persona existente
+    const id = editingRow.querySelector('td:nth-child(1)').textContent;
+    updatePersonRow(editingRow, id, nombre, apellido, edad);
+    editingRow = null;
+    clearInputFields();
+    modal.style.display = 'none';
+  }
+});
+
+function clearInputFields() {
+  document.getElementById('edit-id').value = '';
+  document.getElementById('nombre').value = '';
+  document.getElementById('apellido').value = '';
+  document.getElementById('edad').value = '';
 }
 
-const cantidadPersonas = ()=>{
-    let cantidadDatos = parseInt(prompt("Ingrese el numero de personas que metera en el array"));
-    if(isNaN(cantidadDatos)){
-        alert("Por favor ingrese un valor numerico");
-    }
-    for (let i = 0; i < cantidadDatos; i++){
-        agregarPersonas();
-    }
-}
-cantidadPersonas();
-
-function verPersonas() {
-    let mensaje = "Lista de personas:\n \n";
-  
-    for (let i = 0; i < personas.length; i++) {
-      const persona = personas[i];
-      mensaje += `Nombre: ${persona.nombre}\n`;
-      mensaje += `Apellido: ${persona.apellido}\n`;
-      mensaje += `Edad: ${persona.edad}\n\n`;
-    }
-    alert(mensaje);
+function createPersonRow(id, nombre, apellido, edad) {
+  const newRow = document.createElement('tr');
+  newRow.setAttribute('data-id', id);
+  newRow.innerHTML = `
+    <td>${id}</td>
+    <td>${nombre}</td>
+    <td>${apellido}</td>
+    <td>${edad}</td>
+    <td>
+      <button class="editarBtn" data-id="${id}">Editar</button>
+      <button class="borrarBtn" data-id="${id}">Borrar</button>
+    </td>
+  `;
+  return newRow;
 }
 
-
-const eliminarPersona = () => {
-    const nombre = prompt('Ingrese el nombre de la persona que desea eliminar');
-    const apellido = prompt('Ingrese el apellido de la persona que desea eliminar');
-  
-    const personasFiltradas = personas.filter(p => p.nombre !== nombre || p.apellido !== apellido);
-  
-    if (personasFiltradas.length === personas.length) {
-      alert("No se encontró ninguna persona con el nombre y apellido ingresados en el array.");
-    } else {
-      personas = personasFiltradas;
-      alert(`Se ha eliminado a la persona con nombre ${nombre} y apellido ${apellido} del array.`);
-    }
-  };
+function updatePersonRow(row, id, nombre, apellido, edad) {
+  row.innerHTML = `
+    <td>${id}</td>
+    <td>${nombre}</td>
+    <td>${apellido}</td>
+    <td>${edad}</td>
+    <td>
+      <button class="editarBtn" data-id="${id}">Editar</button>
+      <button class="borrarBtn" data-id="${id}">Borrar</button>
+    </td>
+  `;
+}
